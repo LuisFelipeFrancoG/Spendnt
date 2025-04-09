a496102f226f14a7972573ac7289ac5d92b4d9f5
LuisFelipeFrancoG <luisfelipefranco13@gmail.com>
2025-04-09 13:23:04 -0500

    Correcciones

--- Spendnt.API/Controllers/UsuariosController.cs
+++ Spendnt.API/Controllers/UsuariosController.cs
@@ -1,10 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Spendnt.API.Data;
+using Spendnt.Shared.Entities;
+
 namespace Spendnt.API.Controllers
 {
     [ApiController]
+    [Route("/api/usuarios")]
     public class UsuariosController:ControllerBase
     {
         private readonly DataContext _context;
+
+        public UsuariosController(DataContext context)
+        {
+
+            _context = context;
+
+        }
     }
 }
--- Spendnt.Shared/Entities/Usuario.cs
+++ Spendnt.Shared/Entities/Usuario.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace Spendnt.Shared.Entities
@@ -10,5 +12,27 @@ namespace Spendnt.Shared.Entities
     public class Usuario
     {
         public int Id { get; set; }
+
+        [Display(Name = "Nombre")]
+        [Required]
+        [MaxLength(50)]
+        public string Nombre { get; set; }
+
+        [Display(Name = "Apellido")]
+        [Required]
+        [MaxLength(50)]
+        public string Apellido { get; set; }
+
+        public string NombreCompleto => $"{Nombre} {Apellido}";
+
+        [Display(Name = "Email")]
+        [Required]
+        [MaxLength(100)]
+        public string Email { get; set; }
+
+        [Display(Name = "Contraseña")]
+        [Required]
+        [MaxLength(50)]
+        public string ContraseñaHash { get; set; }
     }
 }
