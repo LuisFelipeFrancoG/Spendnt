fcaf4178b3fada31f74952815a646ae869bf05c5
LuisFelipeFrancoG <luisfelipefranco13@gmail.com>
2025-04-09 12:32:34 -0500

    Correciones

---
+++ Spendnt.API/Controllers/UsuariosController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Spendnt.API.Data;
+namespace Spendnt.API.Controllers
+{
+    [ApiController]
+    public class UsuariosController:ControllerBase
+    {
+        private readonly DataContext _context;
+    }
+}
---
+++ Spendnt.API/Data/DataContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Spendnt.Shared.Entities;
+
+
+namespace Spendnt.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Usuario> Usuarios { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
