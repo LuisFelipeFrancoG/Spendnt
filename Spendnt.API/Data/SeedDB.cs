a5304da3e09cb9c9c0258d27b5a01305c173f8ab
LuisFelipeFrancoG <luisfelipefranco13@gmail.com>
2025-05-16 10:20:42 -0500

    Actualizaciones y SeedDB User

--- Spendnt.API/Data/SeedDB.cs
+++ Spendnt.API/Data/SeedDB.cs
@@ -1,30 +1,61 @@
-﻿using Spendnt.Shared.Entities;
+﻿using Spendnt.API.Helpers;
+using Spendnt.Shared.Entities;
+using Spendnt.Shared.Enums;
 
 namespace Spendnt.API.Data
 {
     public class SeedDB
     {
         private readonly DataContext _context;
+        private readonly IUserHelper _userHelper;
 
-        public SeedDB(DataContext context) 
+        public SeedDB(DataContext context, IUserHelper userHelper) 
         { 
             _context = context;
+            _userHelper = userHelper;
         }
 
         public async Task SeedAsync() 
         {
             await _context.Database.EnsureCreatedAsync();
-            await CheckUsuario();
+            await CheckRolesAsync();
+            await CheckUserAsync("Luis", "Franco", "lff@gmail.com", "300445555", UserType.Admin);
+
         }
 
-        public async Task CheckUsuario()
+        private async Task<User> CheckUserAsync(string Nombre, string Apellido, string email, string Contraseña, UserType userType)
         {
-            if (!_context.Usuarios.Any())
+            var user = await _userHelper.GetUserAsync(email);
+            if (user == null)
             {
-                //_context.Usuarios.Add(new Usuario { Nombre = "Pancho" });
-                //_context.Usuarios.Add(new Usuario { Nombre = "Juancho" });
+                user = new User
+                {
+
+                    Nombre = Nombre,
+                    Apellido = Apellido,
+                    Email = email,
+                    Contraseña = Contraseña,
+                    
+
+
+                    UserName = email,
+
+
+                    UserType = userType,
+                };
+
+                await _userHelper.AddUserAsync(user, "123456");
+                await _userHelper.AddUserToRoleAsync(user, userType.ToString());
             }
-            await _context.SaveChangesAsync();
+
+            return user;
         }
+
+        private async Task CheckRolesAsync()
+        {
+            await _userHelper.CheckRoleAsync(UserType.Admin.ToString());
+            await _userHelper.CheckRoleAsync(UserType.User.ToString());
+        }
+
     }
 }
