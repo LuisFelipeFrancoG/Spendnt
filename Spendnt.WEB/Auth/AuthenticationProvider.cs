59e574d9de8360979fbaa53b505f8b14f37b40b0
LuisFelipeFrancoG <luisfelipefranco13@gmail.com>
2025-05-21 13:04:51 -0500

    Actualizar

---
+++ Spendnt.WEB/Auth/AuthenticationProvider.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
+
+namespace Spendnt.WEB.Auth
+{
+    public class AuthenticationProvider : AuthenticationStateProvider
+    {
+        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            var anonimous = new ClaimsIdentity();
+            var lffgUser = new ClaimsIdentity(new List<Claim>
+        {
+            new Claim("FirstName", "Luis"),
+            new Claim("LastName", "Felipe"),
+            new Claim(ClaimTypes.Name, "luisfelipe@gmail.com"),
+            new Claim(ClaimTypes.Role, "Admin")
+        },
+                authenticationType: "test");
+            return await Task.FromResult(new AuthenticationState(new ClaimsPrincipal(lffgUser)));
+        }
+    }
+}
--- Spendnt.WEB/Program.cs
+++ Spendnt.WEB/Program.cs
@@ -1,11 +1,16 @@
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Spendnt.WEB;
+using Spendnt.WEB.Auth;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddSingleton(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7000") });
+
+builder.Services.AddAuthorizationCore();
+builder.Services.AddScoped<AuthenticationStateProvider, AuthenticationProvider>();
 
 await builder.Build().RunAsync();
