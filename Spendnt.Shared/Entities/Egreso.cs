2a9fa76b419a1c3ded8773c7da304df6a4ef74b4
LuisFelipeFrancoG <luisfelipefranco13@gmail.com>
2025-05-26 14:19:50 -0500

    Egreso

--- Spendnt.Shared/Entities/Egreso.cs
+++ Spendnt.Shared/Entities/Egreso.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Text.Json.Serialization;
@@ -12,25 +13,36 @@ namespace Spendnt.Shared.Entities
     {
         public int Id { get; set; }
 
-        [Required(ErrorMessage = "El monto del egreso es requerido.")]
+        [Required]
         [Display(Name = "Egreso")]
-        [Range(0.01, double.MaxValue, ErrorMessage = "El monto del egreso debe ser mayor que cero.")]
+        [Range(0.01, double.MaxValue)]
         public decimal Egreso { get; set; }
 
-        [Required(ErrorMessage = "La categoría es obligatoria.")]
+        [Required]
+        [Display(Name = "Fecha")]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy HH:mm}", ApplyFormatInEditMode = true)]
+        public DateTime Fecha { get; set; }
+
+        [Required]
+        [Display(Name = "Categoría")]
         public int CategoriaId { get; set; }
 
         [JsonIgnore]
         public Categoria Categoria { get; set; }
 
-        [Required(ErrorMessage = "La fecha es requerida.")]
-        [Display(Name = "Fecha")]
-        public DateTime Fecha { get; set; }
+        [NotMapped]
+        [Required(ErrorMessage = "El nombre de la categoría es requerido para el formulario.")]
+        [StringLength(100, ErrorMessage = "El nombre de la categoría no puede exceder los 100 caracteres.")]
+        [Display(Name = "Nombre de Categoría (para formulario)")]
+        public string NombreCategoria { get; set; } = string.Empty;
 
-        [Required(ErrorMessage = "El saldo es obligatorio.")]
+        [Required]
         public int SaldoId { get; set; }
 
         [JsonIgnore]
         public Saldo Saldo { get; set; }
+
+
+
     }
 }
