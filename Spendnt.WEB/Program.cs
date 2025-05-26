ac1f0b2d2ed01cca890b2cfd2f961fccfb865453
LuisFelipeFrancoG <luisfelipefranco13@gmail.com>
2025-05-26 08:56:36 -0500

    .

--- Spendnt.WEB/Program.cs
+++ Spendnt.WEB/Program.cs
@@ -1,4 +1,3 @@
-using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Spendnt.WEB;
@@ -7,6 +6,6 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddSingleton(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7000") });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
